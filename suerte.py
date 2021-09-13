c88548062c0c696fd1b11fc411fad33f94d8bfe3
Akihayakawa <Khun.17122004@gmail.com>
2021-09-13 12:20:49 -0500

    Se agrega el Proyecto

---
+++ suerte.py
@@ -0,0 +1,33 @@
+import random
+import time 
+lista = ["Piedra","Papel","Tijera"]
+jugador = input("Piedra papel o tijera?: ")
+time.sleep(1)
+ran = random.choice(lista)
+print(ran)
+def piedra():
+    if ran == "Papel":
+        print("has perdido")
+    elif ran == "Tijera":
+        print("Has ganado")
+def papel():
+    if ran == "Piedra":
+        print("Has ganado")
+    elif ran == "Tijera":
+        print("Has perdido")
+
+def tijera():
+    if ran == "Papel":
+        print("Has ganado")
+    elif ran == "Piedra":
+        print("Has perido")
+
+if jugador == ran:
+    print("ha sido un empate")
+elif jugador == "piedra":
+    piedra()
+elif jugador == "tijera" :
+    tijera()
+elif jugador == "papel":
+    papel()
+
